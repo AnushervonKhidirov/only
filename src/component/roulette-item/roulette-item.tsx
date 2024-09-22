e346bdee84db143fbd3fcd809788b8439ddf27c8
AnushervonKhidirov <ankhid98@gmail.com>
2024-09-22 19:49:10 +0500

    roulette modified

--- src/component/roulette-item/roulette-item.tsx
+++ src/component/roulette-item/roulette-item.tsx
@@ -3,13 +3,14 @@ import { useEffect, useRef, useState } from 'react'
 
 import useRouletteStore from '../../store/roulette'
 
+
 import classNames from 'classnames'
 import styles from './roulette-item.module.scss'
 
 type TRouletteItem = {
     totalIndex: number
     index: number
-    title: string
+    title?: string
 }
 
 const RouletteItem: FC<TRouletteItem> = ({ totalIndex, index, title }) => {
@@ -23,11 +24,9 @@ const RouletteItem: FC<TRouletteItem> = ({ totalIndex, index, title }) => {
     const degreeBetweenItems = 360 / totalIndex
 
     useEffect(() => {
-        if (rouletteItemRef.current && circleRef.current && titleRef.current) {
-            rouletteItemRef.current.style.rotate = `${itemDeg - degree}deg`
-            circleRef.current.style.rotate = `${-itemDeg + degree}deg`
-            titleRef.current.style.rotate = `${-itemDeg + degree}deg`
-        }
+        if (rouletteItemRef.current) rouletteItemRef.current.style.rotate = `${itemDeg - degree}deg`
+        if (circleRef.current) circleRef.current.style.rotate = `${-itemDeg + degree}deg`
+        if (titleRef.current) titleRef.current.style.rotate = `${-itemDeg + degree}deg`
     }, [itemDeg, degree])
 
     useEffect(() => {
@@ -52,9 +51,11 @@ const RouletteItem: FC<TRouletteItem> = ({ totalIndex, index, title }) => {
                 <div className={styles.border}></div>
                 <div className={styles.index}>{index + 1}</div>
             </div>
-            <div ref={titleRef} className={styles.rouletteTitle}>
-                {title}
-            </div>
+            {title && (
+                <div ref={titleRef} className={styles.rouletteTitle}>
+                    {title}
+                </div>
+            )}
         </div>
     )
 }
--- src/component/roulette/roulette.tsx
+++ src/component/roulette/roulette.tsx
@@ -1,14 +1,13 @@
 import RouletteItem from '../roulette-item/roulette-item'
 
+import { chapters } from '../chapter-slider/constant'
 import styles from './roulette.module.scss'
-
-const items = ['Наука 1', 'Наука 2', 'Наука 3', 'Наука 4', 'Наука 5', 'Наука 6']
 
 const Roulette = () => {
     return (
         <div className={styles.roulette}>
-            {items.map((item, index, items) => {
-                return <RouletteItem totalIndex={items.length} index={index} title={item} key={item} />
+            {chapters.map((chapter, index, chapters) => {
+                return <RouletteItem totalIndex={chapters.length} index={index} title={chapter} key={`chapter-${index}`} />
             })}
         </div>
     )
