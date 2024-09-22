a86509dbfbeccbb377f0796cf33e895f8edb4d4e
AnushervonKhidirov <ankhid98@gmail.com>
2024-09-22 19:49:27 +0500

    arrow modified

--- src/component/arrow/arrow.tsx
+++ src/component/arrow/arrow.tsx
@@ -6,13 +6,14 @@ import styles from './arrow.module.scss'
 
 type TArrow = {
     side: 'left' | 'right'
-    callback: () => void
+    color?: 'light' | 'dark'
+    callback?: () => void
     className?: string
 }
 
-const Arrow: FC<TArrow> = ({ side, callback, className }) => {
+const Arrow: FC<TArrow> = ({ side, color, callback, className }) => {
     return (
-        <div className={classNames(styles.arrowWrapper, styles[side], className)} onClick={callback}>
+        <div className={classNames(styles.arrowWrapper, styles[side], styles[color], className)} onClick={callback}>
             <div className={styles.arrow}></div>
         </div>
     )
